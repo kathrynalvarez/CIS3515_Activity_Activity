4dacb76cc9362bcdddd955874bed0049d3932dc8
kathrynalvarez <tui78504@temple.edu>
2024-10-01 14:55:54 -0400

    Pass and extract value from intent

--- app/src/main/java/edu/temple/activities/DisplayActivity.kt
+++ app/src/main/java/edu/temple/activities/DisplayActivity.kt
@@ -12,6 +12,8 @@ class DisplayActivity : AppCompatActivity() {
         // TODO Step 3: Extract transferred value and use for lyricsDisplayView text size
         with (findViewById<TextView>(R.id.lyricsDisplayTextView)) {
 
+            val textSize = intent.getIntExtra("textSize", 0)
+            setTextSize(textSize.toFloat())
         }
 
 
--- app/src/main/java/edu/temple/activities/MainActivity.kt
+++ app/src/main/java/edu/temple/activities/MainActivity.kt
@@ -1,5 +1,6 @@
 package edu.temple.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -23,9 +24,14 @@ class MainActivity : AppCompatActivity() {
         with (findViewById<RecyclerView>(R.id.textSizeSelectorRecyclerView)) {
 
             // TODO Step 2: Implement lambda body to launch new activity and pass value
-            adapter = TextSizeAdapter(textSizes){
 
+            val callback = {textSize: Int ->
+                val intent = Intent(this@MainActivity, DisplayActivity::class.java)
+                intent.putExtra("textSize", textSize)
+                startActivity(intent)
             }
+
+            adapter = TextSizeAdapter(textSizes, callback)
             layoutManager = LinearLayoutManager(this@MainActivity)
         }
 
@@ -36,12 +42,14 @@ class MainActivity : AppCompatActivity() {
 
 
 /* Convert to RecyclerView.Adapter */
-class TextSizeAdapter (private val textSizes: Array<Int>, callback: (Int)->Unit) : RecyclerView.Adapter<TextSizeAdapter.TextSizeViewHolder>() {
+class TextSizeAdapter (private val textSizes: Array<Int>, private val callback: (Int)->Unit) : RecyclerView.Adapter<TextSizeAdapter.TextSizeViewHolder>() {
 
     // TODO Step 1: Complete onClickListener to return selected number
     inner class TextSizeViewHolder(val textView: TextView) : RecyclerView.ViewHolder (textView) {
         init {
-            textView.setOnClickListener {  }
+            textView.setOnClickListener {
+                callback(textSizes[adapterPosition])
+            }
         }
     }
 
